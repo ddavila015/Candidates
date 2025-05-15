e0ae9e72c7bab8eb921728d8ee69db3f031748dd
Daniells015 <ddavila015@gmail.com>
2025-05-15 17:23:41 -0500

    se agrega la informacion de las experiencias en una subconsulta de la tabla

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddScoped<ICandidateData, CandidateData>();
 builder.Services.AddScoped<ICandidateDomain, CandidateDomain>();
 builder.Services.AddScoped<ResponseDto>();
 builder.Services.AddScoped<List<CandidateDto>>();
-
+ 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<CandidateContext>(options =>
